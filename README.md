8a8925ae2fcce315f38e18f5fc803983b6c36498
etidbury <tidbury@live.com>
2018-06-17 20:44:39 +0100

    remove unused deps and simplify task specification. Also include some validators

--- README.md
+++ README.md
@@ -4,50 +4,46 @@ A utility script for executing either promises or CLI commands sequentially whil
 <h3>Example Usage</h3>
 
 ```javascript
-require('exec-sequence')(
-    [
-        {
-            text: "Ensure git changes have been committed to branch",
-            command: "git diff --exit-code",
-            options: {cwd: "./"},
-            error: "Make sure you have committed/stashed your changes before trying to update the development branch"
-        },
-        {
-            text: "Test a promise resolve after 5s",
-            promise: new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve("Successfully resolved after 5s");
-                }, 5000);
-            }),
-            error: "Test promise failed to resolve after 5s"
-        },
-        {
-            text: "Test a promise rejection after 10s",
-            promise: new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    reject("No reason what so ever!");
-                }, 10000);
-            }),
-            error: "Don't worry! This task was meant to reject!"
-        },
-        {
-            text: "Test Command 3 (This does not run because the previous task throws an error)",
-            command: "pwd",
-            options: {cwd: "./server"},
-            error: "Maybe sure the 'server' folder exists"
-        },
-    ],{maxStdOutPerTask:10}
-).then(() => {
+require('./')({
+    "Open Google": {
+        command: "open http://google.com/ && exit 0",
+        options: {cwd: "./"},
+        error: "Failed to open Google"
+    },
+    "Ensure git changes have been committed to branch": {
+        command: "git diff --exit-code",
+        options: {cwd: "./"},
+        error: "Make sure you have committed/stashed your changes before trying to update the development branch"
+    },
+    "Test a promise resolve after 5s": {
+        promise: new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Successfully resolved after 5s");
+            }, 5000);
+        }),
+        error: "Test promise failed to resolve after 5s"
+    },
+    "Test a promise rejection after 10s": {
+        promise: new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject("No reason what so ever!");
+            }, 10000);
+        }),
+        error: "Don't worry! This task was meant to reject!"
+    },
+    "Test Command 3 (This does not run because the previous task throws an error)": {
+        command: "pwd",
+        options: {cwd: "./server"},
+        error: "Maybe sure the 'server' folder exists"
+    }
+
+}).then(() => {
     console.log(`All tasks ran successfully!`);
     process.exit(0);
 })
     .catch(({cmd, err}) => {
-        console.error(`Custom handler to spit out error for task '${cmd.text}':`,err);
+        console.error(`Custom handler to spit out error for task '${cmd.text}':`, err);
         process.exit(1);
     });
-
-
-
-
 
 ```
--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-
-
 const MultiSpinner = require('multispinner');
 const chalk = require('chalk');
 const _exec = require('child_process').exec;
-const cliClear=require('cli-clear');
 
-
-/*
-const exec = (command, options) => {
-
-    return new Promise((resolve, reject) => {
-
-        _exec(command, options,(e, stdout, stderr)=> {
-            if (e instanceof Error) {
-                reject(stderr);
-                return;
-            }
-            resolve(stdout);
-        });
-
-    });
-};*/
 const exec = async (command, options) => {
 
-    const e = await _exec(command, Object.assign(options,{}));
+    const e = await _exec(command, Object.assign(options, {}));
 
+    const {stdout, stderr} = e;
+    
+    return new Promise((resolve, reject) => {
 
-    const {stdout,stderr}=e;
-
-
-    return new Promise((resolve,reject)=>{
-
-        const _data=[];
+        const _data = [];
 
         stdout.on('data', function (data) {
             _data.push(data);
@@ -43,36 +22,41 @@ const exec = async (command, options) => {
 
         e.on('exit', function (code) {
 
-            if (code===0){
+            if (code === 0) {
                 resolve(_data.join('\n'))
-            }else{
+            } else {
                 reject(_data.join('\n'))
             }
             //console.log('child process exited with code ' + code.toString());
         });
     });
 
-
 };
 
-module.exports=async (cmds,config) => {
+module.exports = async (_cmds, config) => {
 
-    config=Object.assign({
-        maxStdOutPerTask:10
-    },config);
+    let cmds = [];
+
+    if (!Object.keys(_cmds)||!Object.keys(_cmds).length)
+        throw new Error("No tasks specified");
+
+    Object.keys(_cmds).forEach((text) => {
+        if (!text || !text.length) throw new Error("Invalid task name specified");
+
+        if (typeof _cmds[text].promise ==="undefined"&&typeof _cmds[text].command ==="undefined")
+            throw new Error(`Task '${text}' has no promise or command specified! At least one must be specified`);
+
+        cmds.push(Object.assign(_cmds[text], {text, id: text, _text: text}))
+    });
+
+    config = Object.assign({
+        maxStdOutPerTask: 10
+    }, config);
 
 
     const _msc = {};
 
     let i;
-
-
-    for (let j = 0; j < cmds.length; j++) {
-        cmds[j].id=cmds[j].text+"";
-        cmds[j]._text=cmds[j].text+"";
-
-    }
-
 
     cmds.forEach((cmd) => {
         _msc[cmd.text] = cmd.text;
@@ -80,11 +64,11 @@ module.exports=async (cmds,config) => {
 
     const multispinner = new MultiSpinner(_msc);
 
-    const _origSpinnerText=[];
-    const _appendTextLines=[];
+    const _origSpinnerText = [];
+    const _appendTextLines = [];
 
 
-    multispinner.appendText=function(cmd,text){
+    multispinner.appendText = function (cmd, text) {
 
         //this.spinners[cmd.id]._text=this.spinners[cmd.id].text;
 
@@ -92,66 +76,64 @@ module.exports=async (cmds,config) => {
 
 
         if (!_origSpinnerText[cmd.id])
-            _origSpinnerText[cmd.id]=Object.assign({},cmd).text;
+            _origSpinnerText[cmd.id] = Object.assign({}, cmd).text;
 
 
         if (!_appendTextLines[cmd.id])
-            _appendTextLines[cmd.id]=[];
+            _appendTextLines[cmd.id] = [];
 
-        text=chalk.white(text);//format text
+        text = chalk.white(text);//format text
 
         _appendTextLines[cmd.id].push(text);
 
-        if (_appendTextLines[cmd.id].length>=config.maxStdOutPerTask)//restrict to X lines of text
+        if (_appendTextLines[cmd.id].length >= config.maxStdOutPerTask)//restrict to X lines of text
             _appendTextLines[cmd.id].shift();
 
-        this.spinners[cmd.id].text=cmd._text+("\n"+_appendTextLines[cmd.id].join('\n'));
+        this.spinners[cmd.id].text = cmd._text + ("\n" + _appendTextLines[cmd.id].join('\n'));
     };
 
     //let _activeCMD;
 
-    multispinner.resetText=function(){
+    multispinner.resetText = function () {
 
-        Object.keys(this.spinners).forEach((key)=>{
+        Object.keys(this.spinners).forEach((key) => {
             if (_origSpinnerText[key])
-                this.spinners[key].text= _origSpinnerText[key]+"";
+                this.spinners[key].text = _origSpinnerText[key] + "";
         });
         //this.spinners[_activeCMD.id].text=this.spinners[_activeCMD.id]._text;
     };
 
-    let _oldConsole=[];
-    const overrideConsoles=(j)=>{
+    let _oldConsole = [];
+    const overrideConsoles = (j) => {
 
-        ['warn','log','error'].forEach((f)=>{
+        ['warn', 'log', 'error'].forEach((f) => {
 
             if (!_oldConsole[f])
                 _oldConsole[f] = console[f];
 
             global.console[f] = function () {
 
-                const args=[].slice.apply(arguments);
-                if (typeof j!=="number") {//reset
+                const args = [].slice.apply(arguments);
+                if (typeof j !== "number") {//reset
                     Array.prototype.unshift.call(arguments);
                     _oldConsole[f].apply(this, arguments);
-                }else{
-                    const prettyArgs="\t"+args
-                        .filter((r)=>(typeof r==="string"||typeof r==="object"||typeof r==="number"))
-                        .map((r)=>typeof r==="number"&&r||r.toString())
+                } else {
+                    const prettyArgs = "\t" + args
+                        .filter((r) => (typeof r === "string" || typeof r === "object" || typeof r === "number"))
+                        .map((r) => typeof r === "number" && r || r.toString())
                         .join('  ')
-                        .substr(0,200);
+                        .substr(0, 200);
 
-                    multispinner.appendText(cmds[j],prettyArgs);//todo: format arguments based on type
+                    multispinner.appendText(cmds[j], prettyArgs);//todo: format arguments based on type
                 }
             };
         });
-
 
 
     };
 
 
     let cmdError = false;
-
 
 
     for (i = 0; i < cmds.length; i++) {
@@ -165,23 +147,23 @@ module.exports=async (cmds,config) => {
             overrideConsoles(i);
 
             if (cmd.command && cmd.command.length) {
-                cmd.promise = async ()=>exec(cmd.command, cmd.options).catch((err) => {
+                cmd.promise = async () => exec(cmd.command, cmd.options).catch((err) => {
                     //console.error(err.message);
-                   // console.error(`Command '${cmd.command}' had an error:\n`,err);
+                    // console.error(`Command '${cmd.command}' had an error:\n`,err);
 
-                    let errMessage=(err);
+                    let errMessage = (err);
 
 
-                    let stringShifted="";
+                    let stringShifted = "";
 
-                    errMessage.split('\n').forEach(function(line){
+                    errMessage.split('\n').forEach(function (line) {
                         stringShifted += '\t' + line + '\n';
                     });
 
 
                     //errMessage=errMessage.replace('\n','\n\t');
 
-                    console.error(`${chalk.white(chalk.bold.underline(cmd.command)+" output:\n")} `, chalk.grey(stringShifted), "\n");
+                    console.error(`${chalk.white(chalk.bold.underline(cmd.command) + " output:\n")} `, chalk.grey(stringShifted), "\n");
 
                     throw new Error(`Command '${cmd.command}' had an error`);
                 });
@@ -194,14 +176,12 @@ module.exports=async (cmds,config) => {
             }
             //
 
-            if (!cmd.promise || (typeof cmd.promise !=="function")) {
+            if (!cmd.promise || (typeof cmd.promise !== "function")) {
                 throw new Error(`'promise' field must be a function returning a Promise for task '${cmd.text}'`);
             }
 
 
-
-
-            if (cmd.onInit&&typeof cmd.onInit==="function") {
+            if (cmd.onInit && typeof cmd.onInit === "function") {
 
                 await cmd.onInit();
             }
@@ -218,8 +198,6 @@ module.exports=async (cmds,config) => {
             if (response && cmd.onData && typeof cmd.onData === "function") {
                 cmd.onData(response);
             }
-
-
 
 
         } catch (err) {
@@ -242,14 +220,13 @@ module.exports=async (cmds,config) => {
 
                 }
 
-                let errMessage=(cmdError.err && cmdError.err.message || cmdError.err || "");
+                let errMessage = (cmdError.err && cmdError.err.message || cmdError.err || "");
 
-                if (errMessage.length>300)
-                    errMessage=errMessage.substr(0,300)+"\n...";
+                if (errMessage.length > 300)
+                    errMessage = errMessage.substr(0, 300) + "\n...";
 
 
                 console.error(`\n${(cmdError.err && "\nError:\n") || ""}`, chalk.grey(errMessage), "\n");
-
 
 
                 reject(cmdError);
