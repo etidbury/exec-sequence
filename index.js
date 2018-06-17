43b5640c52467c83842aac2f341a76e860b29802
etidbury <tidbury@live.com>
2018-06-17 19:02:52 +0100

    Changes including a better exec process handler, fixing sequential task executions, and prettifying the CLI output

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
+
 const MultiSpinner = require('multispinner');
 const chalk = require('chalk');
 const _exec = require('child_process').exec;
+const cliClear=require('cli-clear');
 
+
+/*
 const exec = (command, options) => {
 
     return new Promise((resolve, reject) => {
@@ -15,12 +20,59 @@ const exec = (command, options) => {
         });
 
     });
+};*/
+const exec = async (command, options) => {
+
+    const e = await _exec(command, Object.assign(options,{}));
+
+
+    const {stdout,stderr}=e;
+
+
+    return new Promise((resolve,reject)=>{
+
+        const _data=[];
+
+        stdout.on('data', function (data) {
+            _data.push(data);
+        });
+
+        stderr.on('data', function (data) {
+            _data.push(data);
+        });
+
+        e.on('exit', function (code) {
+
+            if (code===0){
+                resolve(_data.join('\n'))
+            }else{
+                reject(_data.join('\n'))
+            }
+            //console.log('child process exited with code ' + code.toString());
+        });
+    });
+
+
 };
 
-module.exports=async (cmds) => {
+module.exports=async (cmds,config) => {
+
+    config=Object.assign({
+        maxStdOutPerTask:10
+    },config);
 
 
     const _msc = {};
+
+    let i;
+
+
+    for (let j = 0; j < cmds.length; j++) {
+        cmds[j].id=cmds[j].text+"";
+        cmds[j]._text=cmds[j].text+"";
+
+    }
+
 
     cmds.forEach((cmd) => {
         _msc[cmd.text] = cmd.text;
@@ -28,18 +80,117 @@ module.exports=async (cmds) => {
 
     const multispinner = new MultiSpinner(_msc);
 
+    const _origSpinnerText=[];
+    const _appendTextLines=[];
+
+
+    multispinner.appendText=function(cmd,text){
+
+        //this.spinners[cmd.id]._text=this.spinners[cmd.id].text;
+
+        //const cmd=cmds[cmds.length-1];
+
+
+        if (!_origSpinnerText[cmd.id])
+            _origSpinnerText[cmd.id]=Object.assign({},cmd).text;
+
+
+        if (!_appendTextLines[cmd.id])
+            _appendTextLines[cmd.id]=[];
+
+        text=chalk.white(text);//format text
+
+        _appendTextLines[cmd.id].push(text);
+
+        if (_appendTextLines[cmd.id].length>=config.maxStdOutPerTask)//restrict to X lines of text
+            _appendTextLines[cmd.id].shift();
+
+        this.spinners[cmd.id].text=cmd._text+("\n"+_appendTextLines[cmd.id].join('\n'));
+    };
+
+    //let _activeCMD;
+
+    multispinner.resetText=function(){
+
+        Object.keys(this.spinners).forEach((key)=>{
+            if (_origSpinnerText[key])
+                this.spinners[key].text= _origSpinnerText[key]+"";
+        });
+        //this.spinners[_activeCMD.id].text=this.spinners[_activeCMD.id]._text;
+    };
+
+    let _oldConsole=[];
+    const overrideConsoles=(j)=>{
+
+        ['warn','log','error'].forEach((f)=>{
+
+            if (!_oldConsole[f])
+                _oldConsole[f] = console[f];
+
+            global.console[f] = function () {
+
+                const args=[].slice.apply(arguments);
+                if (typeof j!=="number") {//reset
+                    Array.prototype.unshift.call(arguments);
+                    _oldConsole[f].apply(this, arguments);
+                }else{
+                    const prettyArgs="\t"+args
+                        .filter((r)=>(typeof r==="string"||typeof r==="object"||typeof r==="number"))
+                        .map((r)=>typeof r==="number"&&r||r.toString())
+                        .join('  ')
+                        .substr(0,200);
+
+                    multispinner.appendText(cmds[j],prettyArgs);//todo: format arguments based on type
+                }
+            };
+        });
+
+
+
+    };
+
+
     let cmdError = false;
 
-    for (let i = 0; i < cmds.length; i++) {
+
+
+    for (i = 0; i < cmds.length; i++) {
         const cmd = cmds[i];
         try {
 
-            cmd.id = cmd.text;
+            //cmd.id = cmd.text;
+
+            multispinner.resetText(cmd);//hide output console for task if success
+
+            overrideConsoles(i);
 
             if (cmd.command && cmd.command.length) {
-                cmd.promise = exec(cmd.command, cmd.options).catch((err) => {
-                    //console.error(err.toString());
-                    throw new Error(`Command '${cmd.command}' had an error:\n ${err.toString()}`);
+                cmd.promise = async ()=>exec(cmd.command, cmd.options).catch((err) => {
+                    //console.error(err.message);
+                   // console.error(`Command '${cmd.command}' had an error:\n`,err);
+
+                    let errMessage=(err);
+
+
+                    let stringShifted="";
+
+                    errMessage.split('\n').forEach(function(line){
+                        stringShifted += '\t' + line + '\n';
+                    });
+
+                    if (stringShifted.length>1000)
+                        stringShifted=stringShifted.substr(0,1000)+"\n...";
+
+
+
+
+
+
+                    //errMessage=errMessage.replace('\n','\n\t');
+
+                    console.error(`${chalk.white(chalk.bold.underline(cmd.command)+" output:\n")} `, chalk.grey(stringShifted), "\n");
+
+                    throw new Error(`Command '${cmd.command}' had an error`);
                 });
 
                 /*if (cmd.onData&&typeof cmd.onData==="function"){
@@ -48,23 +199,38 @@ module.exports=async (cmds) => {
                 }*/
 
             }
-            if (!cmd.promise || !(cmd.promise instanceof Promise)) {
-                throw new Error(`Promise not specified for '${cmd.text}'`);
+            //
+
+            if (!cmd.promise || (typeof cmd.promise !=="function")) {
+                throw new Error(`'promise' field must be a function returning a Promise for task '${cmd.text}'`);
             }
 
-            const response = await cmd.promise.then((c) => {
+
+
+
+            if (cmd.onInit&&typeof cmd.onInit==="function") {
+
+                await cmd.onInit();
+            }
+
+            const response = await cmd.promise().then((c) => {
+
 
                 multispinner.success(cmd.id);
 
                 return c;
             });
 
+
             if (response && cmd.onData && typeof cmd.onData === "function") {
                 cmd.onData(response);
             }
 
 
+
+
         } catch (err) {
+
             multispinner.error(cmd.id);
             cmdError = {cmd, err};
             break;
@@ -76,11 +242,22 @@ module.exports=async (cmds) => {
         setTimeout(() => {
             if (cmdError) {
 
+                overrideConsoles();
+
                 if (cmdError.cmd.error && cmdError.cmd.error.length) {
-                    console.error("\n", chalk.bold(chalk.red(cmdError.cmd.error)));
+                    console.error("\n", chalk.black.bgRed(cmdError.cmd.error));
+
                 }
 
-                console.error(chalk.grey(`\n${(cmdError.err && "\nError:\n") || ""}`), chalk.grey(cmdError.err && cmdError.err.message || cmdError.err || ""), "\n");
+                let errMessage=(cmdError.err && cmdError.err.message || cmdError.err || "");
+
+                if (errMessage.length>300)
+                    errMessage=errMessage.substr(0,300)+"\n...";
+
+
+                console.error(`\n${(cmdError.err && "\nError:\n") || ""}`, chalk.grey(errMessage), "\n");
+
+
 
                 reject(cmdError);
                 return;
